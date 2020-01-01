b29f9ff522f55297872cd2104a6a616593bf904d
ILIYA LESANI <iliyalesani@gmail.com>
2020-01-01 23:52:56 +0330

    Update Startup.cs

--- WinScript.Core/Startup.cs
+++ WinScript.Core/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WinScript.Core.Models;
 using WinScript.Core.Services;
 
 namespace WinScript.Core
@@ -25,6 +28,8 @@ namespace WinScript.Core
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddControllers();
             services.AddTransient<JsonFileProductService>();
         }
 
@@ -49,9 +54,18 @@ namespace WinScript.Core
 
             app.UseAuthorization();
 
+            // Simple API to return JSON
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                //endpoints.MapGet("/products", (context) =>
+                //{
+                //    var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
+                //    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+                //    return context.Response.WriteAsync(json);
+                //});
             });
         }
     }
